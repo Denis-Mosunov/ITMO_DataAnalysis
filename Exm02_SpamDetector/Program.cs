fa9658906aff4584a79b8643a9ead65e0b34685f
Denis-Mosunov <dnsmosunov@gmail.com>
2025-04-28 16:23:15 +0300

    Update Program.cs

--- Exm02_SpamDetector/Program.cs
+++ Exm02_SpamDetector/Program.cs
@@ -10,6 +10,14 @@ namespace SpamDetector
     {
         static void Main(string[] args)
         {
+            //Load sample data
+            var sampleData = new MLModel1.ModelInput()
+            {
+                Content = @"Hi team, Just a reminder that we have our weekly project status meeting tomorrow at 10 AM in Conference Room B. Please bring your status reports and be prepared to discuss the timeline updates. Thanks, Sarah",
+            };
+            //Load model and predict output
+            var result = MLModel1.Predict(sampleData);
+            Console.WriteLine(result.PredictedLabel);
         }
     }
 }
